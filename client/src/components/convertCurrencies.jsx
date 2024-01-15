db5bb263e50bb478b198e24ff35c00aea808c790
Miladepour <epour.milad@gmail.com>
2024-01-15 08:22:47 +0000

    fetch the data from API

--- client/src/components/convertCurrencies.jsx
+++ client/src/components/convertCurrencies.jsx
@@ -1,7 +1,25 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function ConvertCurrencies() {
-return (
+const [currencies, setCurrencies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://blockchain.info/ticker")
+      .then((response) => {
+        const currenciesArray = Object.keys(response.data).map((key) => {
+          return { name: key, ...response.data[key] };
+        });
+        setCurrencies(currenciesArray);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
     <div>
-        <h1>Convert Currencies</h1>
+      <h1>Convert Currencies</h1>
     </div>
-)
+  );
 }
