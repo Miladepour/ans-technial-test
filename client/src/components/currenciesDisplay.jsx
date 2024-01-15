0692beb36e843df461ed266a85fbd6b996647cc8
Miladepour <epour.milad@gmail.com>
2024-01-15 08:11:14 +0000

    initial testof API

--- client/src/components/currenciesDisplay.jsx
+++ client/src/components/currenciesDisplay.jsx
@@ -1,7 +1,28 @@
-export default function currenciesDisplay() {
-    return (
-        <div>
-            <h1>Display Currencies</h1>
-        </div>
-    )
-    }
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+export default function CurrenciesDisplay() {
+  const [currencies, setCurrencies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://blockchain.info/ticker")
+      .then((response) => {
+        const currenciesArray = Object.keys(response.data).map((key) => {
+          return { name: key, ...response.data[key] };
+        });
+        setCurrencies(currenciesArray);
+        console.log(currenciesArray);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+
+    </div>
+  );
+}
