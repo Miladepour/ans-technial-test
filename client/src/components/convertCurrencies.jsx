f2a89b70095bb776758e64a9c8b4952c17a611ad
Miladepour <epour.milad@gmail.com>
2024-01-15 08:26:39 +0000

    create the body of convert exchange form

--- client/src/components/convertCurrencies.jsx
+++ client/src/components/convertCurrencies.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Button, Card, Form } from "react-bootstrap";
 
 export default function ConvertCurrencies() {
 const [currencies, setCurrencies] = useState([]);
@@ -19,7 +20,23 @@ const [currencies, setCurrencies] = useState([]);
   }, []);
   return (
     <div>
-      <h1>Convert Currencies</h1>
+      <Card>
+        <Card.Header as="h5">Currency Converter</Card.Header>
+        <Card.Body>
+          <Card.Title>Enter your amount</Card.Title>
+          <Form.Control type="number" />
+          <p>Select your currency</p>
+          <p>From</p>
+          <Form.Select>
+          </Form.Select>
+          <p>To</p>
+          <Form.Select >
+          </Form.Select>
+          <Button variant="primary" className="mt-2">
+            Convert
+          </Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
